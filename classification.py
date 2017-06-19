f4a612ef2ec21a728c03d5286eeee8bac72a5107
seidenfeder <n.palandt@campus.lmu.de>
2017-06-19 12:33:07 +0200

    add the option -c to give the number of iterations in the cross validation

--- classification.py
+++ classification.py
@@ -3,7 +3,8 @@
 
 ####################################################################################################
 #
-#This script is able to run the two classification methods Support Vector Machine and Random Forest
+# This script is able to run the two classification methods Support Vector Machine and Random Forest
+# Output gives the AUROC scores of the cross validation with the chosen method 
 #
 ####################################################################################################
 
@@ -19,6 +20,7 @@ parser.add_option("-m", type="string", dest="method", help = "the method you wan
 parser.add_option("-i",dest="input", help="This gives the path to the file with the input data (the output of the binning)")
 parser.add_option("-l",dest="labels", help="This gives the path to the file with the labels")
 parser.add_option("-b",type = "int",dest="bin", help="Tells which bin should be used for the classification")
+parser.add_option("-c",type = "int",dest="crossVal", help="Tells in how many parts we divide our dataset for the cross validation", default=5)
 parser.add_option("-a", dest="allBins", help = "Tells if all bins should be used", default=False)
 parser.add_option("-o",dest="output", help="The name of the outputfile", default="classification.txt")
 (options, args) = parser.parse_args()
@@ -79,12 +81,11 @@ if(method=="SVM"):
 elif(method=="RF"):
     clf=RandomForestClassifier(n_estimators=16)
 
-scores = cross_val_score(clf, X, y, cv=10, scoring='roc_auc')
+scores = cross_val_score(clf, X, y, cv=options.crossVal, scoring='roc_auc')
 
 #write the output into a file but don't delete the previous text
 #this is necessary that we can compare different data sets or binnings or methods
 fileHandle = open ( options.output, 'a' )
 fileHandle.write(method+"\t"+'\t'.join(map(str,scores))+"\n")
 fileHandle.close()
-
 
