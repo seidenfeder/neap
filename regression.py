Some Bugs fixed, normalisation of the expression values by log
normalisation of the expression values by log
now a regression can be plotted

regression.py
@@ -6,14 +6,17 @@
 # This script is able to run the three Regression methods:
 # Linear Regression, Support Vector Machine and Random Forest
 # The output is the r2 scores of the cross validation with the chosen method
+# If wanted the regression will be plotted
 #
 ####################################################################################################
 
 import numpy as np
 from sklearn import svm, linear_model
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,cross_val_predict
 from optparse import OptionParser
+from math import log
+import matplotlib.pyplot as plt
 
 #this is necessary to get the parameters from the comand line
 parser = OptionParser()
@@ -22,6 +25,7 @@ parser.add_option("-i",dest="input", help="This gives the path to the file with 
 parser.add_option("-b",type = "int",dest="bin", help="Tells which bin should be used for the classification")
 parser.add_option("-c",type = "int",dest="crossVal", help="Number of iterations in the cross validation", default=5)
 parser.add_option("-a", dest="allBins", help = "Tells if all bins should be used", default=False)
+parser.add_option("-p", dest="plot", help = "True it makes you a plot, Flase(default) it makes no plot", default=False)
 parser.add_option("-o",dest="output", help="The name of the outputfile", default="regression.txt")
 (options, args) = parser.parse_args()
 method=options.method
@@ -48,8 +52,7 @@ for line in featureFile.readlines():
 
         
 
-#Sort labels according to the feature list
-#Maybe for some genes no GENCODE entry could be found, these are only in the features list
+#Get the expression Value and the signal of the histone modifications in the bins
 y=[]
 X=[]
 #If not all bins should be used
@@ -57,15 +60,19 @@ if(not options.allBins):
 	binNumber=options.bin
 	#Create feature matrix of the given bin 
 	for geneID in genesModis:
-	    y.append(labelDict[geneID])
-	    valueMatrix=np.array(genesModis[geneID])
-	    X.append(valueMatrix[:,binNumber])
-#if you want the classification with all bins
+	    val = values[geneID]
+	    if not val==0:
+		    y.append(log(val))
+		    valueMatrix=np.array(genesModis[geneID])
+		    X.append(valueMatrix[:,binNumber])
+#if you want the regression with all bins
 else:
 	for geneID in genesModis:
-	    y.append(labelDict[geneID])
-	    valueMatrix=np.array(genesModis[geneID])
-	    X.append(valueMatrix.flatten())
+	    val = values[geneID]
+	    if not val==0:
+		    y.append(log(val))
+		    valueMatrix=np.array(genesModis[geneID])
+		    X.append(valueMatrix.flatten())
 
 
 #Support Vector Machines
@@ -73,14 +80,26 @@ if(method=="SVM"):
     rg=svm.SVR(cache_size=500)
 #Random Forest
 elif(method=="RF"):
-    rg=RandomForestRegressor(n_estimators=16)
+    rg=RandomForestRegressor(n_estimators=12)
 elif(method=="LR"):
     rg=linear_model.LinearRegression()
 
 #make the cross validation
-scores = cross_val_score(rg, X, y, cv=options.crossVal, scoring='r2')
+scores = cross_val_score(rg, X, y, cv=options.crossVal, scoring="r2")
+
+# plot the Regression if a plot is wanted
+if options.plot:
+    pred = cross_val_predict(rg, X, y, cv=options.crossVal)
+    fig, ax = plt.subplots()
+    ax.scatter(y, pred)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    plt.show()
 
 #write the output into a file but don't delete the previous text
-fileHandle = open ( options.output, 'a' )
-fileHandle.write(method+"\t"+'\t'.join(map(str,scores))+"\n")
+fileHandle = open ( options.output, 'a')
+if(not options.allBins):
+    fileHandle.write(method+"\t"+str(binNumber)+"\t"+'\t'.join(map(str,scores))+"\n")
+else:
+    fileHandle.write(method+"\tall\t"+'\t'.join(map(str,scores))+"\n")
 fileHandle.close()