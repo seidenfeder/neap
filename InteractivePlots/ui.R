c48eb9ad647f42928d70368cbfdfc16ffbdbf321
seidenfeder <nicola_palandt@yahoo.de>
2017-07-26 09:19:07 +0200

    new text on the website

--- InteractivePlots/ui.R
+++ InteractivePlots/ui.R
@@ -8,14 +8,47 @@ shinyUI(
   navbarPage("Gene expression predidiction",theme = shinytheme("sandstone"),
              tabPanel("Project description",
                       fluidPage(
-                        h1("NEAP Group 1 - Gene expression prediction using histone modifications"),
-                        h4("Nicola Palandt, Katharina Schmid"),
-                        h2("Introduction"),
-                        p(paste("On this website we present the interactive plots, created during our research project on ",
-                                "'Gene Expression Prediction using Histone Modifications'. For detailed information about the project,",
-                                "read our report. The most important results are presented shortly in the following tabs.")),
-                        h2("Model development"),
-                        h2("Regression ")
+                        fluidRow(
+                          column(2,
+                                 " "
+                          ),
+                          column(8,
+                                 h1("NEAP Group 1 - Gene expression prediction using histone modifications"),
+                                 h4("Nicola Palandt, Katharina Schmid"),
+                                 h2("Introduction"),
+                                 p(paste("On this website we present the interactive plots, created during our research project on ",
+                                         "'Gene Expression Prediction using Histone Modifications'. We analysed three different cell lines from the ENCODE database ",
+                                         "with a bigger set of histone modifications. The two primary cell lines: the endothelial cell of umbilical vein and the keratinocyte as well as the immortalized cell line K562, ",
+                                         "which is a leukemia cell. For detailed information about the project,",
+                                         "read our report. The most important results are presented shortly in the following tabs.")),
+                                 h2("Model development"),
+                                 p(paste("Here we tried to improve our performance by trying different label method and different normalization methods, ",
+                                         "we also explored which bin the most important bin is.")),
+                                 h2("Regression "),
+                                 p(paste("In this tab you will find the results of the regresssion for different datasets, you will see two scatter plots ",
+                                         "that show the measured and the predicted data once with the expression level zero and once without it.")),
+                                 h2("Spatial Information"),
+                                 p(paste("Here we show the normalized signal pattern of the histone modifications over the different bins, as well as the Spearman correlation ",
+                                         "between the different bins and the gen expression values. We compare these pattern with the performance of each single bin.")),
+                                 h2("Histone Modification"),
+                                 p(paste("Different histone modifications play a differnent role for the prediction. In this tab we compare the importance ",
+                                         "of the different histone modifications.")),
+                                 h2("Dataset Comparision"),
+                                 p(paste("We compared six different datasets with each other, two immortalized cell lines, two tissues and two primary cell lines. ",
+                                         "We run all the datasets with our implemented methods and tested the model with the other datasets")),
+                                 h2("Deep Learning"),
+                                 p(paste("Deep Learning is a method that is very differnet from the other implemented ones, this tab is to show all our result that we got ",
+                                         "from deep learning. Showing the learning curve and ....")),
+                                 h2("Run Prediction"),
+                                 p(paste("In this tab you are able to compare your own dataset with our datasets. You are able to predict the gene expression ",
+                                         "by using our trained models and compare the performance with the performance of our models.
+                                         "))
+                                 
+                          ),
+                          column(2,
+                                 " ")
+                        )
+                        
                       )),
              tabPanel("Model development",
                       sidebarLayout(
@@ -34,6 +67,7 @@ shinyUI(
                                                          "Keratinocyte"="keratinocyte"),
                                              selected = "K562")
                         ),
+                        
                         mainPanel(
                           uiOutput("dynamic")
                         )
