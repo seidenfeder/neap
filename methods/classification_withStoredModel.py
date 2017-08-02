a1d30570700fa472e42b2b00a873fd31a157bdb3
seidenfeder <nicola_palandt@yahoo.de>
2017-08-02 14:08:03 +0200

    a minor mistake changed

--- methods/classification_withStoredModel.py
+++ methods/classification_withStoredModel.py
@@ -19,7 +19,7 @@ parser.add_option("-l",dest="labelsTest", help="This gives the path to the file 
 parser.add_option("-b",type = "int",dest="bin", help="Tells which bin should be used for the classification")
 parser.add_option("-a", action="store_true", dest="allBins", help = "Tells if all bins should be used", default=False)
 parser.add_option("-m",dest="modelFile", help="Model file where the trained classificator is saved in.")
-parser.add_option("-o",dest="output", help="The name of the outputfile", default="regression.txt")
+parser.add_option("-o",dest="output", help="The name of the outputfile", default="classification_pred.txt")
 parser.add_option("-n", action="store_true", dest="newFormat", help="Feature file created by bins annotated, containing ENCODE metadata infos", default=False)
 parser.add_option("-t",dest="trainset", help="The name of the trainset", default="")
 
